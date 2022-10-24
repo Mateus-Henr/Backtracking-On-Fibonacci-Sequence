b7191abf2b5b97665216a96b4f15ec9fc1c23b8c
Mateus-Henr <mateush.study@gmail.com>
2022-10-24 10:16:34 -0300

    Update on way of reading file into matrix and update stuff

--- arquivo.c
+++ arquivo.c
@@ -1,87 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <stddef.h>
+#include <limits.h>
 
-int fibonacci(int n) {
-    if (n == 0) {
-        return 0;
-    } else if (n == 1) {
-        return 1;
-    } else {
-        return fibonacci(n - 1) + fibonacci(n - 2);
-    }
+int fibonacci(int n)
+{
+    return (int) (pow(((1 + sqrt(5)) / 2), n) + pow(((1 - sqrt(5)) / 2), n));
 }
 
 
-int **createMatrix(int rows, int cols, char *filename) {
-
+int **initializeMatrix(int rows, int cols)
+{
     int **matrix = (int **) malloc(rows * sizeof(int *));
-    for (int i = 0; i < rows; i++) {
+
+    if (!matrix)
+    {
+        return NULL;
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
         matrix[i] = (int *) malloc(cols * sizeof(int));
     }
 
-    FILE *file = fopen(filename, "r");
-    int m, n;
-    fscanf(file, "%d %d", &m, &n);
-
-    
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            fscanf(file, "%d", &matrix[i][j]);
-        }
-    }
-
-    fclose(file);
     return matrix;
 }
 
-int main() {
-    int cols, rows;
-    char filename[100];
-    FILE *file;
-
-    printf("Digite o nome do arquivo: ");
-    scanf("%s", filename);
-
-    file = fopen(filename, "r");
-
-    if (file == NULL) {
-        printf("Erro ao abrir o arquivo");
-        return 1;
-    }
-
-    fscanf(file, "%d %d", &rows, &cols);
-    fclose(file);
-    
-
-    int **matrix = createMatrix(rows, cols, filename);
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%d ", matrix[i][j]);
+void printMatrix(int **matrix, int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("[%02d] ", matrix[i][j]);
         }
+
         printf("\n");
     }
+}
 
-    
-    int x;
-    printf("Type a number: ");
-    scanf("%d", &x);
-    printf("\nElement %d: %d", x, fibonacci(x));
+int main()
+{
+    char filename[CHAR_MAX];
 
-    printf("\nSequence of fibonacci, from 0 to %d: ", x);
-    for (int i = 0; i <= x; i++) {
+    printf("Type the filename: ");
+    scanf("%s", filename);
+
+    FILE *file = fopen(filename, "r");
+
+    if (!file)
+    {
+        printf("ERROR: Couldn't open the file.");
+        return -1;
+    }
+
+    int cols, rows;
+    fscanf(file, "%d %d", &rows, &cols);
+
+    int **matrix = initializeMatrix(rows, cols);
+
+    if (!matrix)
+    {
+        return -1;
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            fscanf(file, "%d ", &matrix[i][j]);
+        }
+    }
+
+
+    int number = 0;
+
+    printf("\nType a number: ");
+    scanf("%d", &number);
+
+    printf("\nElement %d: %d", number, fibonacci(number));
+    printf("\nSequence of fibonacci, from 0 to %d: ", number);
+    for (int i = 0; i <= number; i++)
+    {
         printf("%d ", fibonacci(i));
     }
 
-    printf("\n");
+    printf("\nPrinting out the matrix\n");
+    printMatrix(matrix, rows, cols);
 
     return 0;
-
-
-
-
-
-    
- 
 }
 
--- readme.txt
+++
@@ -1,1 +0,0 @@
-first commit my first commit
