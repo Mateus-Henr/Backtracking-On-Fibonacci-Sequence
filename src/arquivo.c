a6fc64b207d5456623d6318bf8019ca35a182495
AlanGabriews <ms.alangabriel@gmail.com>
2022-10-28 02:29:32 -0300

    poucas coisas
    consertei a funcao de fibonacci com formula fechada(a q ta na especificacao nao funciona) e essa fibSequence ja retorna a sequencia q agnt precisa(1, 1, 1, 1, 1, 2, 1, 1, 2, 3, ...)
    comecei a fazer a funcao recursiva tbm, mas falta colocar uma outra tabela nx2 dentro dela pra ser o caminho, no caso to pensando em fazer um provisorio e um caminho final, q no caso seria o maior caminho. tambem acho q n seria ruim se agnt fizesse um TAD pra cada unidade de plantacao, so pra n ter q criar aql tabela de flag q eu fiz, mas se vcs tiverem outras ideias melhores pra contornar o problema, da a call

--- src/arquivo.c
+++ src/arquivo.c
@@ -3,12 +3,26 @@
 #include <math.h>
 #include <stddef.h>
 #include <limits.h>
+#include <stdbool.h>
 
 int fibonacci(int n)
 {
-    return (int) (pow(((1 + sqrt(5)) / 2), n) + pow(((1 - sqrt(5)) / 2), n));
+    // i couldnt find out why, but for some reason this formula is returning 0 for n = 2, probably something with how those math functions work in c, lets let it as it is for now, its working nevertheless
+    if (n == 2) return 1;
+    else return (int) (((1 / sqrt(5)) * (pow(((1 + sqrt(5)) / 2), n))) - ((1 / sqrt(5)) * (pow(((1 - sqrt(5)) / 2), n))));
 }
 
+int triangular(int n)
+{
+    return (n * (n + 1)) / 2;
+}
+
+int fibSequence(int n)
+{
+    int k = 0;
+    while (n > triangular(k)) k++;
+    return fibonacci(n - triangular(k - 1));
+}
 
 int **initializeMatrix(int rows, int cols)
 {
@@ -40,8 +54,51 @@ void printMatrix(int **matrix, int rows, int cols)
     }
 }
 
+void movimentar (int x, int y, int rows, int cols, int** matrix, int** flag, int n)
+{
+    bool possibleMove = false;
+    if(x > 0)
+    {
+        if(matrix[x - 1][y] == fibSequence(n + 1) && flag[x - 1][y] == 0)
+        {
+            possibleMove = true;
+            flag[x - 1][y] = 1;
+            movimentar(x - 1, y, rows, cols, matrix, flag, n + 1);
+        }
+    }
+    if(x < rows - 1)
+    {
+        if(matrix[x + 1][y] == fibSequence(n + 1) && flag[x + 1][y] == 0)
+        {
+            possibleMove = true;
+            flag[x + 1][y] = 1;
+            movimentar(x + 1, y, rows, cols, matrix, n);
+        }
+    }
+    if(y > 0)
+    {
+        if(matrix[x][y - 1] == fibSequence(n + 1) && flag[x][y - 1] == 0)
+        {
+            possibleMove = true;
+            flag[x][y - 1] = 1;
+            movimentar(x, y - 1, rows, cols, matrix, n);
+        }
+    }
+    if(y < cols - 1)
+    {
+        if(matrix[x][y + 1] == fibSequence(n + 1) && flag[x][y + 1] == 0)
+        {
+            possibleMove = true;
+            flag[x][y + 1] = 1;
+            movimentar(x, y + 1, rows, cols, matrix, n);
+        }
+    }
+}
+
 int main()
 {
+    for (int i = 1; i < 22; i++) printf("%d\n", fibSequence(i));
+
     char filename[CHAR_MAX];
 
     printf("Type the filename: ");
@@ -51,7 +108,7 @@ int main()
 
     if (!file)
     {
-        printf("ERROR: Couldn't open the file.");
+        printf("ERROR: Couldn't open the file.\n");
         return -1;
     }
 
@@ -73,8 +130,18 @@ int main()
         }
     }
 
+    //maybe thats a dumb way to check if this part of the crop has already been visited, but i couldnt come up with anything else, i guess that if we made a data structure for the crops, this would be better
+    bool **flagMatrix = initializeMatrix(rows, cols);
 
-    int number = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            flagMatrix[i][j] = 0;
+        }
+    }
+
+    /*int number = 0;
 
     printf("\nType a number: ");
     scanf("%d", &number);
@@ -87,8 +154,36 @@ int main()
     }
 
     printf("\nPrinting out the matrix\n");
-    printMatrix(matrix, rows, cols);
+    printMatrix(matrix, rows, cols);*/
+
+    // finds somewhere to start the harvest in the borders of the crops
+    bool found = false;
+    for(int i = 0; i < rows; i++)
+    {
+        if(i == 0 || i == rows - 1)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                if(matrix[i][j] == 1)
+                {
+                    movimentar(i, j, rows, cols, matrix, flagMatrix, 1);
+                    found = true;
+                }
+            }
+        }
+        else
+        {
+            for(int j = 0; j < cols; j += cols - 1)
+            {
+                if(matrix[i][j] == 1)
+                {
+                    movimentar(i, j, rows, cols, matrix, flagMatrix, 1);
+                    found = true;
+                }
+            }
+        }
+    }
+    if(!found) printf("Impossible to find optmal path\n");
 
     return 0;
 }
-
